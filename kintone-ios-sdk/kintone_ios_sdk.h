0b060b9964eb75ad26e139d64453a7e7bab73a12
hiroyuki-sekiyama <hiroyuki-sekiyama@cybozu.co.jp>
2018-09-10 11:40:41 +0900

    first commit

---
+++ kintone-ios-sdk/kintone_ios_sdk.h
@@ -0,0 +1,19 @@
+//
+//  kintone_ios_sdk.h
+//  kintone-ios-sdk
+//
+//  Created by y001112 on 2018/09/04.
+//  Copyright © 2018年 y001112. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for kintone_ios_sdk.
+FOUNDATION_EXPORT double kintone_ios_sdkVersionNumber;
+
+//! Project version string for kintone_ios_sdk.
+FOUNDATION_EXPORT const unsigned char kintone_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kintone_ios_sdk/PublicHeader.h>
+
+
