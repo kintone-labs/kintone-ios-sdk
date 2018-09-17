ebb30c083732d9e724c1db896d17dfcb04ed145f
Hiroyuki Sekiyama <hiroyuki-sekiyama@cybozu.co.jp>
2018-09-18 06:59:14 +0900

    SSR-198/SSR-201 implimention : create  LanguageSetting.swift/BulkRequestConstants.swift/BulkRequestItem.swift/BulkRequestModel.swift/BulkRequestResponse.swift

---
+++ kintone-ios-sdk/Classes/model/bulkrequest/BulkRequestConstants.swift
@@ -0,0 +1,18 @@
+//
+//  BulkRequestConstants.swift
+//  kintone-ios-sdk
+//
+//  Created by h001218 on 2018/09/14.
+//  Copyright © 2018年 Cybozu. All rights reserved.
+//
+
+public class BulkRequestConstants: NSObject {
+    public static let ADD_RECORD: String = "AddRecordRequest";
+    public static let ADD_RECORDS: String = "AddRecordsRequest";
+    public static let UPDATE_RECORD: String = "UpdateRecordRequest";
+    public static let UPDATE_RECORDS: String = "UpdateRecordsRequest";
+    public static let DELETE_RECORDS: String = "DeleteRecordsRequest";
+    public static let UPDATE_STATUS_RECORD: String = "UpdateRecordStatusRequest";
+    public static let UPDATE_STATUS_RECORDS: String = "UpdateRecordsStatusRequest";
+    public static let UPDATE_ASSIGNEE_RECORDS: String = "UpdateRecordAssigneesRequest";
+}
---
+++ kintone-ios-sdk/Classes/model/bulkrequest/BulkRequestModel.swift
@@ -0,0 +1,24 @@
+//
+//  BulkRequestModel.swift
+//  kintone-ios-sdk
+//
+//  Created by h001218 on 2018/09/17.
+//  Copyright © 2018年 Cybozu. All rights reserved.
+//
+
+public class BulkRequestModel: NSObject, Codable {
+    private var requests: Array<BulkRequestItem>? = []
+    
+    public func getRequests() -> Array<BulkRequestItem>? {
+        return self.requests
+    }
+    
+    public func addRequests(_ bulkRequestItem: BulkRequestItem) {
+        self.requests?.append(bulkRequestItem)
+    }
+    
+    public override init() {
+        self.requests = Array<BulkRequestItem>();
+        super.init()
+    }
+}
