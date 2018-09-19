8047fb2ce481b8eed92be59b38f7eba5689bbe3a
tomoya takaki <tomoya-takaki@cybozu.co.jp>
2018-09-19 11:50:39 +0900

    SSR-194 implimention : crate auth constant class

---
+++ kintone-ios-sdk/Classes/authentication/AuthenticationConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationConstants.swift
+//  kintone-ios-sdk
+//
+//  Created by t000572 on 2018/09/19.
+//  Copyright © 2018年 Cybozu. All rights reserved.
+//
+
+import Foundation
+
+public class AuthenticationConstants: NSObject {
+    public static let HEADER_KEY_AUTH_PASSWORD = "X-Cybozu-Authorization"
+    public static let HEADER_KEY_AUTH_APITOKEN = "X-Cybozu-API-Token"
+    public static let HEADER_KEY_AUTH_BASIC = "Authorization"
+    public static let AUTH_BASIC_PREFIX = "Basic "
+}
