bc5f0b784a9fe255e31eabdd944f24e649294ca7
trung-doan <trung-doan@cybozu.vn>
2018-09-19 14:56:05 +0700

    Init App Module structure

---
+++ kintone-ios-sdk/Classes/module/app/AppCustomization.swift
@@ -0,0 +1,16 @@
+//
+//  AppCustomization.swift
+//  kintone-ios-sdk
+//
+//  Created by trung-doan on 9/19/18.
+//  Copyright © 2018 Cybozu. All rights reserved.
+//
+
+protocol AppCustomization {
+    
+}
+
+extension AppCustomization where Self: App {
+    
+}
+
---
+++ kintone-ios-sdk/Classes/module/app/AppForm.swift
@@ -0,0 +1,15 @@
+//
+//  AppForm.swift
+//  kintone-ios-sdk
+//
+//  Created by trung-doan on 9/19/18.
+//  Copyright © 2018 Cybozu. All rights reserved.
+//
+
+protocol AppForm {
+
+}
+
+extension AppForm where Self: App {
+
+}
