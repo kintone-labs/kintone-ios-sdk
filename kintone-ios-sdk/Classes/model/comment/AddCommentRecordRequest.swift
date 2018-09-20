dbfff763bc292859f7d88d1f8496e025135fe369
youtr-s <suzuki_yutaro@comture.com>
2018-09-20 13:23:43 +0900

    SSR-199 implementation : create comment model classes

---
+++ kintone-ios-sdk/Classes/model/comment/AddCommentRecordRequest.swift
@@ -0,0 +1,29 @@
+//
+//  AddCommentRecordRequest.swift
+//  kintone-ios-sdk
+//
+//  Created by y001112 on 2018/09/19.
+//  Copyright © 2018年 Cybozu. All rights reserved.
+//
+
+import UIKit
+
+class AddCommentRecordRequest: NSObject, Codable {
+    
+    private var app: Int?
+    private var record: Int?
+    private var comment: CommentContent?
+    
+    /// Constructor
+    ///
+    /// - Parameters:
+    ///   - app: the ID of kintone app
+    ///   - record: the ID of record
+    ///   - comment: the content of comment
+    public init(_ app: Int?, _ record: Int?, _ comment: CommentContent?) {
+        self.app = app;
+        self.record = record;
+        self.comment = comment;
+    }
+
+}
---
+++ kintone-ios-sdk/Classes/model/comment/CommentContent.swift
@@ -0,0 +1,44 @@
+//
+//  CommentContent.swift
+//  kintone-ios-sdk
+//
+//  Created by y001112 on 2018/09/19.
+//  Copyright © 2018年 Cybozu. All rights reserved.
+//
+
+import UIKit
+
+class CommentContent: NSObject, Codable {
+    
+    private var text: String?
+    private var mentions: [CommentMention]?
+    
+    /// get the comment content including line feed codes
+    ///
+    /// - Returns: the comment content including line feed codes
+    public func getText() -> String? {
+        return self.text;
+    }
+    
+    /// set the comment content including line feed codes
+    ///
+    /// - Parameter text: the comment content including line feed codes
+    public func setText(_ text: String){
+        self.text = text
+    }
+    
+    /// set an Array including infomation of the comment creator
+    ///
+    /// - Parameter mentions: an Object including infomation of the comment creator
+    public func getMentions() -> [CommentMention]? {
+        return self.mentions
+    }
+    
+    /// set an Array including infomation of the comment creator
+    ///
+    /// - Parameter mentions: an Object including infomation of the comment creator
+    public func setMentions(_ mentions: [CommentMention]?){
+        self.mentions = mentions
+    }
+
+}
