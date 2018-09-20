ba4b5d0e4bdebd8139b6a1b8db7293a02af27cd9
anh-trinh <anh-trinh@cybozu.vn>
2018-09-20 13:17:32 +0700

    Add IconFIle class

--- kintone-ios-sdk/Classes/model/app/general/Icon.swift
+++ kintone-ios-sdk/Classes/model/app/general/Icon.swift
@@ -7,5 +7,25 @@
 //
 
 class Icon: NSObject {
-    private var file: IconFile
+    private var file: IconFile?
+    private var key: String?
+    private var iconType: IconType?
+    
+    public enum IconType: String {
+        case FILE
+        case PRESET
+    }
+    
+    public func getFile() -> IconFile? {
+        return self.file
+    }
+    public func setFile(_ file: IconFile) {
+        self.file = file
+    }
+    public func getKey() -> String? {
+        return self.key
+    }
+    public func setKey(_ key: String) {
+        self.key = key
+    }
 }
