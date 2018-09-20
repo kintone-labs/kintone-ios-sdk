982fb47a7f96f72c9474cc1a0449bfe136f60a5a
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-09-20 16:37:52 +0700

    Update Model  - Layout  - Related Record  - System

---
+++ kintone-ios-sdk/Classes/model/app/form/field/Field.swift
@@ -0,0 +1,72 @@
+//
+//  FormField.swift
+//  kintone-ios-sdk
+//
+//  Created by Pham Anh Quoc Phien on 9/18/18.
+//  Copyright © 2018 Cybozu. All rights reserved.
+//
+
+import Foundation
+
+public class Field: NSObject {
+    internal var code: String = ""
+    internal var type: FieldType?
+    
+
+    /// get
+    ///
+    /// - Returns: code
+    public func getCode() -> String {
+        return self.code
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter code: <#code description#>
+    public func setCode(_ code: String) {
+        self.code = code;
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Returns: <#return value description#>
+    public func getType() -> FieldType? {
+        return self.type
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter type: <#type description#>
+    public func setType(_ type: FieldType) {
+        self.type = type
+    }
+    
+//    public int hashCode() {
+//    final int prime = 31;
+//    int result = 1;
+//    result = prime * result + ((code == null) ? 0 : code.hashCode());
+//    return result;
+//    }
+    public func equals(_ obj: AnyObject?) -> Bool {
+        if (obj == nil) {
+            return false
+        }
+        if (!(obj is Field)) {
+            return false
+        }
+        let other = obj as! Field
+        if (self.code.isEmpty) {
+            if (!other.code.isEmpty) {
+                return false
+            }
+        } else if (!(self.code == other.code)) {
+            return false
+        }
+        return true
+    }
+    
+}
+
