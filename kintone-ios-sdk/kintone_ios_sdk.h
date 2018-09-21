2a0e3c881b89c59f3797077de583c19dee7a7ee2
trung-doan <trung-doan@cybozu.vn>
2018-09-21 13:59:14 +0700

    Remove UIKit

--- kintone-ios-sdk/kintone_ios_sdk.h
+++ kintone-ios-sdk/kintone_ios_sdk.h
@@ -6,6 +6,8 @@
 //  Copyright © 2018年 y001112. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+
 //! Project version number for kintone_ios_sdk.
 FOUNDATION_EXPORT double kintone_ios_sdkVersionNumber;
 
