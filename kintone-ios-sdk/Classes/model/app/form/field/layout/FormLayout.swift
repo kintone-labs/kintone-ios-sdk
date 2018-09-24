fbfca12c8b7ee8d2b30a9168e960471bd823eedd
anh-trinh <anh-trinh@cybozu.vn>
2018-09-24 16:51:13 +0700

    Decode Json to ItemLayout

--- kintone-ios-sdk/Classes/model/app/form/field/layout/FormLayout.swift
+++ kintone-ios-sdk/Classes/model/app/form/field/layout/FormLayout.swift
@@ -45,5 +45,36 @@ public class FormLayout: NSObject, Codable {
     public func setLayout(_ layout: [ItemLayout]?) {
         self.layout = layout
     }
+    
+    enum LayoutsKey: CodingKey {
+        case layout
+    }
+    
+    enum LayoutTypeKey: CodingKey {
+        case type
+    }
+    
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: LayoutsKey.self)
+        var layoutsArrayForType = try container.nestedUnkeyedContainer(forKey: LayoutsKey.layout)
+        var layouts = [ItemLayout]()
+        
+        var layoutsArray = layoutsArrayForType
+        print(layoutsArray)
+        while(!layoutsArrayForType.isAtEnd)
+        {
+            let layout = try layoutsArrayForType.nestedContainer(keyedBy: LayoutTypeKey.self)
+            let type = try layout.decode(LayoutType.self, forKey: LayoutTypeKey.type)
+            switch type {
+            case .ROW:
+                print("found row")
+                layouts.append(try layoutsArray.decode(RowLayout.self))
+            case .SUBTABLE:
+                print("found subtable")
+            case .GROUP:
+                print("found group")
+            }
+        }
+        self.layout = layouts
+    }
 }
-
