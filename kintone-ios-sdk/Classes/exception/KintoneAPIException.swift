c5ca653dd88d5a761dd5ce8245360fd7510e0ded
Hiroyuki Sekiyama <hiroyuki-sekiyama@cybozu.co.jp>
2018-09-25 04:48:08 +0900

    SSR-207 implimention :modify ErrorResponse class/KintoneAPIException class

--- kintone-ios-sdk/Classes/exception/KintoneAPIException.swift
+++ kintone-ios-sdk/Classes/exception/KintoneAPIException.swift
@@ -6,43 +6,70 @@
 //  Copyright © 2018年 Cybozu. All rights reserved.
 //
 
-/**
- * Exception will occur in using kintone RestAPI.
- *
- */
+/// Exception will occur in using kintone RestAPI.
 public class KintoneAPIException: Error {
     private var message: String?
     private var httpErrorCode: Int?
     private var errorResponse: ErrorResponse?
-    private var errorRedponses: Array<ErrorResponse>?
+    private var request: Array<BulkRequestItem>?
+    private var errorResponses: Array<ErrorResponse>?
     
+    /// Constructor
+    ///
+    /// - Parameters:
+    ///   - httpErrorCode: Http error code
+    ///   - errorResponse: Response message sent by kintone RestAPI
     public init(_ httpErrorCode: Int?, _ errorResponse: ErrorResponse?){
         self.message = errorResponse?.getMessage()
         self.httpErrorCode = httpErrorCode
         self.errorResponse = errorResponse
     }
     
+    /// Constructor
+    ///
+    /// - Parameters:
+    ///   - httpErrorCode: Http error code
+    ///   - request: Http request
+    ///   - errorResponses: Response message sent by kintone RestAPI
+    public init(_ httpErrorCode: Int?, _ request: Array<BulkRequestItem>?, _ errorResponses: Array<ErrorResponse>? ) {
+        self.httpErrorCode = httpErrorCode
+        self.errorResponses = errorResponses
+        self.request = request
+    }
+    
+    /// Constructor
+    ///
+    /// - Parameter error: The error code which is returned from the kintone API
     public init(_ error: String?){
         self.message = error
     }
     
+    /// get Http error code
+    ///
+    /// - Returns: Http error code
     public func getHttpErrorCode() -> Int? {
         return self.httpErrorCode
     }
     
+    /// get Error Response message
+    ///
+    /// - Returns: Response message sent by kintone RestAPI
     public func getErrorResponse() -> ErrorResponse? {
         return self.errorResponse
     }
     
+    /// output error message
+    ///
+    /// - Returns: Error message
     public func toString() -> String? {
         if (self.errorResponse == nil){
-            if (self.errorRedponses == nil){
+            if (self.request == nil || self.errorResponses == nil){
                 return message
             }
         }
         
         var sb: String = ""
-        if (self.errorRedponses == nil){
+        if (self.errorResponses == nil){
             if (errorResponse?.getId() != nil){
                 sb.append("id: " + (errorResponse?.getId())!)
             }
@@ -52,10 +79,27 @@ public class KintoneAPIException: Error {
             if (errorResponse?.getMessage() != nil){
                 sb.append(", message: " + (errorResponse?.getMessage())!)
             }
+            if (errorResponse?.getErrors() != nil){
+                sb.append(", errors: " + (errorResponse?.getErrors())!)
+            }
         } else {
             var count: Int = 1
+            if (errorResponses != nil) {
+                for errorResponse in errorResponses! {
+                    if (errorResponse.getId() != nil) {
+                        sb.append("api_no: " + String(count))
+                        sb.append(", method: " + (self.request?[count - 1].getMethod())!)
+                        sb.append(", api_name: " + (self.request?[count - 1].getApi())!)
+                        sb.append(", id: " + errorResponse.getId()!)
+                        sb.append(", code: " + errorResponse.getCode()!)
+                        sb.append(", message: " + errorResponse.getMessage()!)
+                        sb.append(", errors: " + errorResponse.getErrors()!)
+                    }
+                    count += 1
+                }
+            }
         }
-        
+        sb.append(", status: \(String(describing: self.httpErrorCode))")
         return sb
     }
 }
