dca1c9aa4291a8ac32f2a894cad88b11d2ef499c
Hiroyuki Sekiyama <hiroyuki-sekiyama@cybozu.co.jp>
2018-09-26 10:09:27 +0900

    SSR-205 :modify BulkRequest class

--- kintone-ios-sdk/Classes/module/bulkrequest/BulkRequest.swift
+++ kintone-ios-sdk/Classes/module/bulkrequest/BulkRequest.swift
@@ -25,10 +25,198 @@ class BulkRequest: NSObject {
     ///   - app: <#app description#>
     ///   - record: <#record description#>
     /// - Returns: <#return value description#>
-    public func addRecord(_ app: Int, _ record: [String: FieldValue]) -> BulkRequest {
+    public func addRecord(_ app: Int?, _ record: [String: FieldValue]) -> BulkRequest {
         let addRecordRequest: AddRecordRequest = AddRecordRequest(app, record)
         do {
             let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.POST_REQUEST, connection.getPathURI(ConnectionConstants.RECORD), addRecordRequest as Any)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Add multi records.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - records: <#records description#>
+    /// - Returns: <#return value description#>
+    public func addRecords(_ app: Int?, _ records: Array<[String: FieldValue]?>) -> BulkRequest {
+        
+        var tempRecords: Array<[String: FieldValue]> = []
+        for record in records {
+        //for (HashMap<String, FieldValue> record : records) {
+            if (record == nil) {
+                tempRecords.append([:])
+            } else {
+                tempRecords.append(record!)
+            }
+        }
+        
+        let addRecordRequest: AddRecordsRequest = AddRecordsRequest(app, tempRecords)
+        do {
+            let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.POST_REQUEST, connection.getPathURI(ConnectionConstants.RECORDS), addRecordRequest as Any)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Update the specific record by ID.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - id: <#id description#>
+    ///   - record: <#record description#>
+    ///   - revision: <#revision description#>
+    /// - Returns: <#return value description#>
+    public func updateRecordByID(_ app: Int?, _ id: Int?, _ record: [String: FieldValue]?, _ revision: Int?) -> BulkRequest {
+        let updateRecordRequest: UpdateRecordRequest = UpdateRecordRequest(app, id, nil, revision, record)
+        do {
+            let bulkRequestItem: BulkRequestItem  = try BulkRequestItem(ConnectionConstants.PUT_REQUEST, connection.getPathURI(ConnectionConstants.RECORD), updateRecordRequest as Any)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Update the specific record by updateKey.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - updateKey: <#updateKey description#>
+    ///   - record: <#record description#>
+    ///   - revision: <#revision description#>
+    /// - Returns: <#return value description#>
+    public func updateRecordByUpdateKey(_ app: Int?, _ updateKey: RecordUpdateKey?, _ record: [String: FieldValue]?, _ revision: Int) -> BulkRequest {
+        let updateRecordRequest: UpdateRecordRequest  = UpdateRecordRequest(app, nil, updateKey, revision, record)
+        do {
+            let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.PUT_REQUEST, connection.getPathURI(ConnectionConstants.RECORD), updateRecordRequest)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Update multi records.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - records: <#records description#>
+    /// - Returns: <#return value description#>
+    public func updateRecords(_ app: Int?, _ records: Array<RecordUpdateItem>? ) -> BulkRequest {
+        let updateRecordsRequest: UpdateRecordsRequest = UpdateRecordsRequest(app, records)
+        do {
+            let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.PUT_REQUEST, connection.getPathURI(ConnectionConstants.RECORDS), updateRecordsRequest)
+                self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    
+    }
+    
+    /// Delete multi records.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - ids: <#ids description#>
+    /// - Returns: <#return value description#>
+    public func deleteRecords(_ app: Int?, _ ids: Array<Int>?) -> BulkRequest {
+        let deleteRecordsRequest: DeleteRecordsRequest = DeleteRecordsRequest(app, ids, nil)
+        do {
+            let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.DELETE_REQUEST, connection.getPathURI(ConnectionConstants.RECORDS), deleteRecordsRequest)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Delete records at the specific revision.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - idsWithRevision: <#idsWithRevision description#>
+    /// - Returns: <#return value description#>
+    public func deleteRecordsWithRevision(_ app: Int?, _ idsWithRevision: [Int: Int]) -> BulkRequest {
+    
+        var ids: Array<Int> = []
+        var revisions: Array<Int> = []
+    
+        for entry in idsWithRevision {
+            ids.append(entry.key)
+            revisions.append(entry.value)
+        }
+        
+        let deleteRecordsRequest: DeleteRecordsRequest = DeleteRecordsRequest(app, ids, revisions)
+        
+        do {
+            let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.DELETE_REQUEST, connection.getPathURI(ConnectionConstants.RECORDS), deleteRecordsRequest)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Update assignees of the specific record.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - record: <#record description#>
+    ///   - assignees: <#assignees description#>
+    ///   - revision: <#revision description#>
+    /// - Returns: <#return value description#>
+    public func updateRecordAssignees(_ app: Int?, _ record: Int?, _ assignees: Array<String>?, _ revision: Int?) -> BulkRequest {
+    
+        let updateRecordAssigneesRequest: UpdateRecordAssigneesRequest = UpdateRecordAssigneesRequest(app, record, assignees, revision)
+        do {
+            let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.PUT_REQUEST, connection.getPathURI(ConnectionConstants.RECORD_ASSIGNEES), updateRecordAssigneesRequest)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Update status of the specific record.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - id: <#id description#>
+    ///   - action: <#action description#>
+    ///   - assignee: <#assignee description#>
+    ///   - revision: <#revision description#>
+    /// - Returns: <#return value description#>
+    public func updateRecordStatus(_ app: Int?, _ id: Int?, _ action: String?, _ assignee: String?, _ revision: Int?) -> BulkRequest {
+    
+        let updateRecordStatusRequest: UpdateRecordStatusRequest = UpdateRecordStatusRequest(action, app, assignee, id, revision)
+        
+        do {
+            let bulkRequestItem: BulkRequestItem = try BulkRequestItem(ConnectionConstants.PUT_REQUEST, connection.getPathURI(ConnectionConstants.RECORD_STATUS), updateRecordStatusRequest)
+            self.bulkRequests.addRequest(bulkRequestItem)
+            return self
+        } catch {
+            return self
+        }
+    }
+    
+    /// Update status of the multi records.
+    ///
+    /// - Parameters:
+    ///   - app: <#app description#>
+    ///   - records: <#records description#>
+    /// - Returns: <#return value description#>
+    public func updateRecordsStatus(_ app: Int?, _ records: Array<RecordUpdateStatusItem>?) -> BulkRequest {
+    
+        let updateRecordsStatusRequest: UpdateRecordsStatusRequest = UpdateRecordsStatusRequest(app, records)
+        do {
+            let bulkRequestItem: BulkRequestItem  = try BulkRequestItem(ConnectionConstants.PUT_REQUEST, connection.getPathURI(ConnectionConstants.RECORDS_STATUS), updateRecordsStatusRequest)
             self.bulkRequests.addRequest(bulkRequestItem)
             return self
         } catch {
@@ -41,55 +229,72 @@ class BulkRequest: NSObject {
     /// - Returns: BulkRequestResponse
     /// - Throws: KintoneAPIExcdeption
     public func execute() throws -> BulkRequestResponse {
-        let parser = RecordParser()
+        let parser = BulkRequestParser()
         let responses: BulkRequestResponse = BulkRequestResponse();
         do {
-            var requests: Array<BulkRequestItem>  = self.bulkRequests.getRequests()!
+            let requests: Array<BulkRequestItem>  = self.bulkRequests.getRequests()!
     
             let body = try parser.parseObject(self.bulkRequests)
             let jsonBody = String(data: body, encoding: String.Encoding.utf8)
         
-            let response: Data = try connection.request(ConnectionConstants.POST_REQUEST, ConnectionConstants.BULK_REQUEST, jsonBody!);
+            let response: Data = try connection.request(ConnectionConstants.POST_REQUEST, ConnectionConstants.BULK_REQUEST, jsonBody!)
+            let jsonobject = try JSONSerialization.jsonObject(with: response, options: JSONSerialization.ReadingOptions.allowFragments)
+            
+            let jsonArray = (jsonobject as! NSDictionary)["results"] as! NSArray
+            
+            var count: Int = 0
+            for request in requests {
+                let jsonRequest: Data = try JSONSerialization.data(withJSONObject: jsonArray[count], options: [])
+                
+                var apiName: NSString = request.getApi()! as NSString
+                let regex = try NSRegularExpression(pattern: "/v1/(.*).json", options: NSRegularExpression.Options())
+                if let regex_result = regex.firstMatch(in: apiName as String, options: NSRegularExpression.MatchingOptions(), range: NSMakeRange(0, apiName.length)){
+                    
+                    apiName = "\(apiName.substring(with: regex_result.range(at: 1)))" as NSString
+                } else {
+                    throw KintoneAPIException("Missing search target character string")
+                }
+                switch (apiName as String) + ":" + request.getMethod()! {
+                    case "\(ConnectionConstants.RECORD):\(ConnectionConstants.POST_REQUEST)":
+                        let res: AddRecordResponse = try parser.parseJson(AddRecordResponse.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    case "\(ConnectionConstants.RECORDS):\(ConnectionConstants.POST_REQUEST)":
+                        let res: AddRecordsResponse = try parser.parseJson(AddRecordsResponse.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    case "\(ConnectionConstants.RECORD):\(ConnectionConstants.PUT_REQUEST)":
+                        let res: UpdateRecordRequest = try parser.parseJson(UpdateRecordRequest.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    case "\(ConnectionConstants.RECORDS):\(ConnectionConstants.PUT_REQUEST)":
+                        let res: UpdateRecordsRequest = try parser.parseJson(UpdateRecordsRequest.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    case "\(ConnectionConstants.RECORDS):\(ConnectionConstants.DELETE_REQUEST)":
+                        let res: DeleteRecordsRequest = try parser.parseJson(DeleteRecordsRequest.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    case "\(ConnectionConstants.RECORD_STATUS):\(ConnectionConstants.PUT_REQUEST)":
+                        let res: UpdateRecordStatusRequest = try parser.parseJson(UpdateRecordStatusRequest.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    case "\(ConnectionConstants.RECORDS_STATUS):\(ConnectionConstants.PUT_REQUEST)":
+                        let res: UpdateRecordsStatusRequest = try parser.parseJson(UpdateRecordsStatusRequest.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    case "\(ConnectionConstants.RECORD_ASSIGNEES):\(ConnectionConstants.PUT_REQUEST)":
+                        let res: UpdateRecordAssigneesRequest = try parser.parseJson(UpdateRecordAssigneesRequest.self, jsonRequest)
+                        responses.addResponse(res)
+                        break
+                    default:
+                        throw KintoneAPIException("Invalid Request Command");
+                }
+                count += 1
+            }
         } catch {
             throw KintoneAPIException(error.localizedDescription)
         }
-        /**
-    JsonObject object = response.getAsJsonObject();
-    JsonArray array = object.getAsJsonArray("results");
-    
-    Integer count = 0;
-    for(BulkRequestItem request : requests) {
-    switch(request.getPayload().getClass().getSimpleName()) {
-    case BulkRequestConstants.ADD_RECORD:
-    responses.addResponse(parser.parseJson(array.get(count), AddRecordResponse.class));
-    break;
-    case BulkRequestConstants.ADD_RECORDS:
-    responses.addResponse(parser.parseJson(array.get(count), AddRecordsResponse.class));
-    break;
-    case BulkRequestConstants.UPDATE_RECORD:
-    responses.addResponse(parser.parseJson(array.get(count), UpdateRecordResponse.class));
-    break;
-    case BulkRequestConstants.UPDATE_RECORDS:
-    responses.addResponse(parser.parseJson(array.get(count), UpdateRecordsResponse.class));
-    break;
-    case BulkRequestConstants.DELETE_RECORDS:
-    responses.addResponse(parser.parseJson(array.get(count), HashMap.class));
-    break;
-    case BulkRequestConstants.UPDATE_STATUS_RECORD:
-    responses.addResponse(parser.parseJson(array.get(count), UpdateRecordResponse.class));
-    break;
-    case BulkRequestConstants.UPDATE_STATUS_RECORDS:
-    responses.addResponse(parser.parseJson(array.get(count), UpdateRecordsResponse.class));
-    break;
-    case BulkRequestConstants.UPDATE_ASSIGNEE_RECORDS:
-    responses.addResponse(parser.parseJson(array.get(count), UpdateRecordResponse.class));
-    break;
-    default:
-    throw new KintoneAPIException("Invalid Request Command");
-    }
-    count++;
-    }
- */
     return responses
     }
 }
