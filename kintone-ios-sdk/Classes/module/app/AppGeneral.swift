f418531ac00e0804e60331548601709112e11f88
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-10-06 17:21:14 +0700

    Handle Error for Module

--- kintone-ios-sdk/Classes/module/app/AppGeneral.swift
+++ kintone-ios-sdk/Classes/module/app/AppGeneral.swift
@@ -25,24 +25,36 @@ public protocol AppGeneral {
 
 public extension AppGeneral where Self: App {
     func getGeneralSettings(_ app: Int?, _ lang: LanguageSetting? = nil, _ isPreview: Bool? = false) throws -> GeneralSettings {
-        let getGeneralSettingsRequest = GetGeneralSettingsRequest(app)
-        let body = try! parser.parseObject(getGeneralSettingsRequest)
-        let jsonBody = String(data: body, encoding: String.Encoding.utf8)!
-        
-        let url = (isPreview! ? ConnectionConstants.APP_SETTINGS_PREVIEW : ConnectionConstants.APP_SETTINGS)
-        let response = try! self.connection?.request(ConnectionConstants.GET_REQUEST, url, jsonBody)
-        let generalSettings = try parser.parseJson(GeneralSettings.self, response!)
-        return generalSettings
+        do {
+            let getGeneralSettingsRequest = GetGeneralSettingsRequest(app)
+            let body = try parser.parseObject(getGeneralSettingsRequest)
+            let jsonBody = String(data: body, encoding: String.Encoding.utf8)!
+            
+            let url = (isPreview! ? ConnectionConstants.APP_SETTINGS_PREVIEW : ConnectionConstants.APP_SETTINGS)
+            let response = try self.connection?.request(ConnectionConstants.GET_REQUEST, url, jsonBody)
+            let generalSettings = try parser.parseJson(GeneralSettings.self, response!)
+            return generalSettings
+        } catch let error as KintoneAPIException {
+            throw error
+        } catch {
+            throw KintoneAPIException(error.localizedDescription)
+        }
     }
     
     func updateGeneralSettings(_ app: Int?, _ generalSettings: GeneralSettings?, _ revision: Int? = -1) throws -> BasicResponse {
-        let updateGeneralSettingsRequest = UpdateGeneralSettingsRequest(generalSettings)
-        updateGeneralSettingsRequest.setApp(app)
-        updateGeneralSettingsRequest.setRevision(revision)
-        let body = try! parser.parseObject(updateGeneralSettingsRequest)
-        let jsonBody = String(data: body, encoding: String.Encoding.utf8)!
-        let response = try! self.connection?.request(ConnectionConstants.PUT_REQUEST, ConnectionConstants.APP_SETTINGS_PREVIEW, jsonBody)
-        let basicResponse = try parser.parseJson(BasicResponse.self, response!)
-        return basicResponse
+        do {
+            let updateGeneralSettingsRequest = UpdateGeneralSettingsRequest(generalSettings)
+            updateGeneralSettingsRequest.setApp(app)
+            updateGeneralSettingsRequest.setRevision(revision)
+            let body = try parser.parseObject(updateGeneralSettingsRequest)
+            let jsonBody = String(data: body, encoding: String.Encoding.utf8)!
+            let response = try self.connection?.request(ConnectionConstants.PUT_REQUEST, ConnectionConstants.APP_SETTINGS_PREVIEW, jsonBody)
+            let basicResponse = try parser.parseJson(BasicResponse.self, response!)
+            return basicResponse
+        } catch let error as KintoneAPIException {
+            throw error
+        } catch {
+            throw KintoneAPIException(error.localizedDescription)
+        }
     }
 }
--- kintone-ios-sdk/Classes/module/app/AppView.swift
+++ kintone-ios-sdk/Classes/module/app/AppView.swift
@@ -27,26 +27,30 @@ public extension AppView where Self: App {
     func getViews(_ app: Int?, _ lang: LanguageSetting?,_ isPreview: Bool? = false) throws -> GetViewsResponse {
         do {
             let getViewsRequest = GetViewsRequest(app!, lang!)
-            let body = try! self.parser.parseObject(getViewsRequest)
+            let body = try self.parser.parseObject(getViewsRequest)
             let jsonBody = String(data: body, encoding: String.Encoding.utf8)!
             let apiName = isPreview ?? false ? ConnectionConstants.APP_VIEWS : ConnectionConstants.APP_VIEWS_PREVIEW
-
+            
             let response = try self.connection?.request(ConnectionConstants.GET_REQUEST, apiName, jsonBody)
             return try self.parser.parseJson(GetViewsResponse.self, response!)
         } catch let error as KintoneAPIException {
             throw error
+        } catch {
+            throw KintoneAPIException(error.localizedDescription)
         }
     }
     
     func updateViews(_ app: Int?, _ views: [String: ViewModel],_ revision: Int? = -1) throws -> UpdateViewsResponse {
         do {
             let updateViewsRequest = UpdateViewsRequest(app!, views, revision ?? -1)
-            let body = try! self.parser.parseObject(updateViewsRequest)
+            let body = try self.parser.parseObject(updateViewsRequest)
             let jsonBody = String(data: body, encoding: String.Encoding.utf8)!
             let response = try self.connection?.request(ConnectionConstants.PUT_REQUEST, ConnectionConstants.APP_VIEWS_PREVIEW, jsonBody)
             return try self.parser.parseJson(UpdateViewsResponse.self, response!)
         } catch let error as KintoneAPIException {
             throw error
+        } catch {
+            throw KintoneAPIException(error.localizedDescription)
         }
     }
 }
