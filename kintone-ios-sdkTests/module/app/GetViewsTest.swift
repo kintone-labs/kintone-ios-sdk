3391e236167d8fb9d20c3323138910f0506a2497
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-10-09 16:24:03 +0700

    Update test case for Get Views/ Update Views App

--- kintone-ios-sdkTests/module/app/GetViewsTest.swift
+++ kintone-ios-sdkTests/module/app/GetViewsTest.swift
@@ -1,33 +1,66 @@
 //
-//  GetViewsTest.swift
-//  kintone-ios-sdkTests
-//
-//  Created by Pham Anh Quoc Phien on 10/8/18.
 //  Copyright © 2018 Cybozu. All rights reserved.
-//
 
 import XCTest
+import kintone_ios_sdk
 
 class GetViewsTest: XCTestCase {
+    private let USERNAME = "Phien"
+    private let PASSWORD = "Phien"
+    private let APP_ID = 1686
+    private let LANG = LanguageSetting.EN
+    
+    private var app: App? = nil
+    private var connection: Connection? = nil
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var auth = Auth.init()
+        auth = auth.setPasswordAuth(self.USERNAME, self.PASSWORD)
+        self.connection = Connection(TestsConstants.DOMAIN, auth)
+        self.app = App(self.connection!)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testGetViewsSuccess() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let isPreview: Bool = true
+        var appViewResponse: GetViewsResponse? = nil
+        
+        XCTAssertNoThrow(appViewResponse = try self.app?.getViews(self.APP_ID, self.LANG, isPreview))
+        
+        let results: [String:ViewModel]? = appViewResponse?.getViews()!
+        
+        XCTAssertNotNil(results)
+        XCTAssertEqual(2, results?.count)
+        
+        var viewEntry: ViewModel? = nil
+        XCTAssertNotNil(viewEntry = results!["ViewTest"])
+        XCTAssertEqual("ViewTest", viewEntry?.getName()!)
+        XCTAssertEqual("Record_number desc", viewEntry?.getSort()!)
+        XCTAssertEqual("Created_datetime = LAST_WEEK()", viewEntry?.getFilterCond()!)
+        XCTAssertNotNil(viewEntry?.getId())
+        XCTAssertEqual(ViewModel.ViewType.LIST, viewEntry?.getType()!)
+        
+        XCTAssertEqual(3, viewEntry?.getFields()!.count)
+        
+        let fieldsViewsExpected: [String] = ["Text", "Text_Area", "Created_datetime"]
+        XCTAssertEqual(fieldsViewsExpected, viewEntry?.getFields()!)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testGetViewsFailWhenAppIDNotExist() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let appId: Int = 99999
+        let isPreview: Bool = true
+        
+        XCTAssertThrowsError(try self.app!.getViews(appId, self.LANG, isPreview))
+        {
+            error in XCTAssert(type(of: error) == KintoneAPIException.self)
         }
     }
-
 }
