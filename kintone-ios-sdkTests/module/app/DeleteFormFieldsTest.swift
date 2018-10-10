58e63b91dcd640a123f63df3e218b503909da8e7
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-10-10 18:15:24 +0700

    Unit Test  - Update Form Fields  - Add Form Fields  - Delete Form Fields  - Get Form Fields

--- kintone-ios-sdkTests/module/app/DeleteFormFieldsTest.swift
+++ kintone-ios-sdkTests/module/app/DeleteFormFieldsTest.swift
@@ -1,32 +1,76 @@
-//
-//  DeleteFormFieldsTest.swift
-//  kintone-ios-sdkTests
-//
-//  Created by Pham Anh Quoc Phien on 10/8/18.
 //  Copyright © 2018 Cybozu. All rights reserved.
-//
 
 import XCTest
+import kintone_ios_sdk
 
 class DeleteFormFieldsTest: XCTestCase {
-
+    private let USERNAME = "Phien"
+    private let PASSWORD = "Phien"
+    private let APP_ID = 1698
+    private let LANG = LanguageSetting.EN
+    private let REVISIONs = -1
+    private var app: App? = nil
+    private var connection: Connection? = nil
+    private let fieldCodeTest: String = "Delete_Text"
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var auth = Auth.init()
+        auth = auth.setPasswordAuth(self.USERNAME, self.PASSWORD)
+        self.connection = Connection(TestsConstants.DOMAIN, auth)
+        self.app = App(self.connection!)
+        let singleFielđTest = SingleLineTextField(fieldCodeTest)
+        singleFielđTest.setDefaultValue("Test Field")
+        singleFielđTest.setLabel("Test_Field")
+        singleFielđTest.setRequired(true)
+        singleFielđTest.setUnique(false)
+        singleFielđTest.setExpression("")
+        singleFielđTest.setCode(fieldCodeTest)
+        
+        // Add Update Field object into dictionary with key is Field Code
+        var properties = [String: Field]()
+        properties[fieldCodeTest] = singleFielđTest
+        do {
+            try self.app?.addFormFields(self.APP_ID, properties)
+        } catch let error as KintoneAPIException {
+            print(error)
+        } catch {
+            
+        }
+        
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        do {
+            var fieldCodes: [String] = [String]()
+            fieldCodes.append(fieldCodeTest)
+            try self.app?.deleteFormFields(self.APP_ID, fieldCodes)
+        } catch let error as KintoneAPIException {
+            print(error)
+        } catch {
+            
+        }
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testDeleteFormFieldsSuccess()
+    {
+        var fieldCodes: [String] = [String]()
+        fieldCodes.append(fieldCodeTest)
+        var basicResponse: BasicResponse? = nil
+        XCTAssertNoThrow(basicResponse = try self.app?.deleteFormFields(self.APP_ID, fieldCodes))
+        XCTAssertNotNil(basicResponse?.getRevision())
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testDeleteFormFieldsFailWhenAppIDNotExist()
+    {
+        let appId = 99999
+        var fieldCodes: [String] = [String]()
+        fieldCodes.append(fieldCodeTest)
+        
+        XCTAssertThrowsError(try self.app?.deleteFormFields(appId, fieldCodes))
+        {
+            error in XCTAssert(type(of: error) == KintoneAPIException.self)
         }
     }
 
--- kintone-ios-sdkTests/module/app/UpdateFormFieldsTest.swift
+++ kintone-ios-sdkTests/module/app/UpdateFormFieldsTest.swift
@@ -1,33 +1,63 @@
-//
-//  UpdateFormFieldsTest.swift
-//  kintone-ios-sdkTests
-//
-//  Created by Pham Anh Quoc Phien on 10/8/18.
 //  Copyright © 2018 Cybozu. All rights reserved.
-//
 
 import XCTest
+import kintone_ios_sdk
 
 class UpdateFormFieldsTest: XCTestCase {
+    private let USERNAME = "Phien"
+    private let PASSWORD = "Phien"
+    private let APP_ID = 1698
+    private let LANG = LanguageSetting.EN
+    private let REVISIONs = -1
+    private var app: App? = nil
+    private var connection: Connection? = nil
+    private let fieldCode: String = "Text"
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var auth = Auth.init()
+        auth = auth.setPasswordAuth(self.USERNAME, self.PASSWORD)
+        self.connection = Connection(TestsConstants.DOMAIN, auth)
+        self.app = App(self.connection!)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testUpdateFormFieldsSuccess()
+    {
+        // Create Field Object to Update
+        let updateField = SingleLineTextField(fieldCode)
+        updateField.setDefaultValue("Hello Kintone")
+        updateField.setRequired(true)
+        
+        // Add Update Field object into dictionary with key is Field Code
+        var properties = [String: Field]()
+        properties[fieldCode] = updateField
+        
+        var basicResponse: BasicResponse? = nil
+        XCTAssertNoThrow(basicResponse = try self.app?.updateFormFields(self.APP_ID, properties))
+        XCTAssertNotNil(basicResponse?.getRevision())
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testUpdateFormFieldsFailWhenAppIDNotExist()
+    {
+        let appId = 99999
+        // Create Field Object to Update
+        let updateField = SingleLineTextField(fieldCode)
+        updateField.setDefaultValue("Hello Kintone")
+        updateField.setRequired(true)
+        
+        // Add Update Field object into dictionary with key is Field Code
+        var properties = [String: Field]()
+        properties[fieldCode] = updateField
+        
+        XCTAssertThrowsError(try self.app?.updateFormFields(appId, properties))
+        {
+            error in XCTAssert(type(of: error) == KintoneAPIException.self)
         }
     }
+  
 
 }
