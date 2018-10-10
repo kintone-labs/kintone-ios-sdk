6231a3f8b6519501c58e52767a6721dd864dccd6
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-10-10 15:49:52 +0700

    Unit testing  - DeployAppSettingsTest  -  AddPreviewAppTest  -  GetAppDeployStatusTest

--- kintone-ios-sdkTests/module/app/DeployAppSettingsTest.swift
+++ kintone-ios-sdkTests/module/app/DeployAppSettingsTest.swift
@@ -1,32 +1,43 @@
-//
-//  deployAppSettingsTest.swift
-//  kintone-ios-sdkTests
-//
-//  Created by Pham Anh Quoc Phien on 10/8/18.
 //  Copyright © 2018 Cybozu. All rights reserved.
-//
 
 import XCTest
+import kintone_ios_sdk
 
 class DeployAppSettingsTest: XCTestCase {
-
+    private let USERNAME = "Phien"
+    private let PASSWORD = "Phien"
+    private let APP_ID: Int = 1693
+    private let REVISION: Int = -1
+    private var app: App? = nil
+    private var connection: Connection? = nil
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var auth = Auth.init()
+        auth = auth.setPasswordAuth(self.USERNAME, self.PASSWORD)
+        self.connection = Connection(TestsConstants.DOMAIN, auth)
+        self.app = App(self.connection!)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testDeployAppSettingsSuccess() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let appPreview: AddPreviewAppResponse? = AddPreviewAppResponse(self.APP_ID, self.REVISION)
+        XCTAssertNoThrow(try self.app?.deployAppSettings([appPreview!]))
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testDeployAppSettingsFailWhenAppIDNotExist() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let appID: Int = 99999
+        let appPreview: AddPreviewAppResponse? = AddPreviewAppResponse(appID, self.REVISION)
+        XCTAssertThrowsError(try self.app?.deployAppSettings([appPreview!]))
+        {
+            error in XCTAssert(type(of: error) == KintoneAPIException.self)
         }
     }
 
--- kintone-ios-sdkTests/module/app/GetAppDeployStatusTest.swift
+++ kintone-ios-sdkTests/module/app/GetAppDeployStatusTest.swift
@@ -1,33 +1,53 @@
-//
-//  GetAppDeployStatusTest.swift
-//  kintone-ios-sdkTests
-//
-//  Created by Pham Anh Quoc Phien on 10/8/18.
 //  Copyright © 2018 Cybozu. All rights reserved.
-//
 
 import XCTest
+import kintone_ios_sdk
 
 class GetAppDeployStatusTest: XCTestCase {
+    private let USERNAME = "Phien"
+    private let PASSWORD = "Phien"
+    private let APP_IDS: [Int] = [1693, 1691]
+    private let APP_STATUS: [String] = ["PROCESSING", "SUCCESS", "FAIL", "CANCEL"]
+    private var app: App? = nil
+    private var connection: Connection? = nil
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var auth = Auth.init()
+        auth = auth.setPasswordAuth(self.USERNAME, self.PASSWORD)
+        self.connection = Connection(TestsConstants.DOMAIN, auth)
+        self.app = App(self.connection!)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testGetAppDeployStatusSuccess() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        var deployStatusReponse: GetAppDeployStatusResponse? = nil
+        XCTAssertNoThrow(deployStatusReponse = try self.app?.getAppDeployStatus(self.APP_IDS))
+        XCTAssertEqual(2, deployStatusReponse?.getApps()!.count)
+        
+        var listAppsDeployStatus: [AppDeployStatus] = [AppDeployStatus]()
+        XCTAssertNotNil(listAppsDeployStatus = (deployStatusReponse?.getApps())!)
+        
+        for appDeployStatus in listAppsDeployStatus {
+            XCTAssertTrue(self.APP_IDS.contains(appDeployStatus.getApp()!))
+            let deploytStatus = appDeployStatus.getStatus()!.rawValue
+            XCTAssertTrue(self.APP_STATUS.contains(deploytStatus))
         }
     }
+    
+    func testGetAppFormLayoutFailWhenAppIDNotExist()
+    {
+        let appIds: [Int] = [99999]
+        XCTAssertThrowsError(try self.app?.getAppDeployStatus(appIds))
+        {
+            error in XCTAssert(type(of: error) == KintoneAPIException.self)
+        }
+    }
+    
 
 }
