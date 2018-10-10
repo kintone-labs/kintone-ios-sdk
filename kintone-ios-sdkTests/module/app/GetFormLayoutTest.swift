63c63a292add1443961ae30e7bfecc7a9ddef1c7
Pham Anh Quoc Phien <quocphien90@gmail.com>
2018-10-10 10:03:37 +0700

    Update Get FormLayout / Update form layout

--- kintone-ios-sdkTests/module/app/GetFormLayoutTest.swift
+++ kintone-ios-sdkTests/module/app/GetFormLayoutTest.swift
@@ -1,33 +1,150 @@
-//
-//  GetFormLayoutTest.swift
-//  kintone-ios-sdkTests
-//
-//  Created by Pham Anh Quoc Phien on 10/8/18.
 //  Copyright © 2018 Cybozu. All rights reserved.
-//
 
 import XCTest
+import kintone_ios_sdk
 
 class GetFormLayoutTest: XCTestCase {
+    private let USERNAME = "Phien"
+    private let PASSWORD = "Phien"
+    private let APP_ID = 1687
+    private let LANG = LanguageSetting.EN
+    
+    private var app: App? = nil
+    private var connection: Connection? = nil
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var auth = Auth.init()
+        auth = auth.setPasswordAuth(self.USERNAME, self.PASSWORD)
+        self.connection = Connection(TestsConstants.DOMAIN, auth)
+        self.app = App(self.connection!)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testGetAppFormLayoutSuccess()
+    {
+        var formLayoutResponse: FormLayout? = nil
+        XCTAssertNoThrow(formLayoutResponse = try self.app?.getFormLayout(self.APP_ID))
+        XCTAssertNotNil(formLayoutResponse?.getRevision())
+        
+        var itemLayouts: [ItemLayout]? = [ItemLayout]()
+        XCTAssertNotNil(itemLayouts = formLayoutResponse?.getLayout())
+        XCTAssertGreaterThan(itemLayouts!.count, 0 )
+        var fields: [FieldLayout]? = nil
+        for itemLayout in itemLayouts!
+        {
+            var itemLayoutType: LayoutType? = nil
+            XCTAssertNotNil(itemLayoutType = itemLayout.getType()!)
+            XCTAssertTrue(type(of: itemLayoutType!) == LayoutType.self)
+            switch itemLayoutType!
+            {
+            case LayoutType.ROW:
+                var itemRowLayout: RowLayout? = nil
+                XCTAssertNoThrow(itemRowLayout = itemLayout as? RowLayout)
+                XCTAssertNotNil(fields = itemRowLayout?.getFields())
+                XCTAssertGreaterThanOrEqual((fields?.count)!, 1)
+                break
+            case LayoutType.SUBTABLE:
+                var itemSubTableLayout: SubTableLayout? = nil
+                XCTAssertNoThrow(itemSubTableLayout = itemLayout as? SubTableLayout)
+                XCTAssertNotNil(fields = itemSubTableLayout?.getFields())
+                XCTAssertGreaterThanOrEqual((fields?.count)!, 1)
+                XCTAssertNotNil(itemSubTableLayout?.getCode())
+                break
+            case LayoutType.GROUP:
+                var layout: [RowLayout]? = nil
+                var itemGroupLayout: GroupLayout? = nil
+                XCTAssertNoThrow(itemGroupLayout = itemLayout as? GroupLayout)
+                XCTAssertNotNil(itemGroupLayout?.getCode())
+                XCTAssertNotNil(layout = itemGroupLayout?.getLayout())
+                XCTAssertGreaterThanOrEqual((layout?.count)!, 0)
+                testGroupLayoutSuccess(layout)
+                break
+            }
         }
     }
-
+    
+    func testGetPreLiveAppFormLayoutSuccess()
+    {
+        let isPreview = true
+        var formLayoutResponse: FormLayout? = nil
+        XCTAssertNoThrow(formLayoutResponse = try self.app?.getFormLayout(self.APP_ID, isPreview))
+        XCTAssertNotNil(formLayoutResponse?.getRevision())
+        
+        var itemLayouts: [ItemLayout]? = [ItemLayout]()
+        XCTAssertNotNil(itemLayouts = formLayoutResponse?.getLayout())
+        XCTAssertGreaterThan(itemLayouts!.count, 0 )
+        var fields: [FieldLayout]? = nil
+        for itemLayout in itemLayouts!
+        {
+            var itemLayoutType: LayoutType? = nil
+            XCTAssertNotNil(itemLayoutType = itemLayout.getType()!)
+            XCTAssertTrue(type(of: itemLayoutType!) == LayoutType.self)
+            switch itemLayoutType!
+            {
+            case LayoutType.ROW:
+                var itemRowLayout: RowLayout? = nil
+                XCTAssertNoThrow(itemRowLayout = itemLayout as? RowLayout)
+                XCTAssertNotNil(fields = itemRowLayout?.getFields())
+                XCTAssertGreaterThanOrEqual((fields?.count)!, 1)
+                break
+            case LayoutType.SUBTABLE:
+                var itemSubTableLayout: SubTableLayout? = nil
+                XCTAssertNoThrow(itemSubTableLayout = itemLayout as? SubTableLayout)
+                XCTAssertNotNil(fields = itemSubTableLayout?.getFields())
+                XCTAssertGreaterThanOrEqual((fields?.count)!, 1)
+                XCTAssertNotNil(itemSubTableLayout?.getCode())
+                break
+            case LayoutType.GROUP:
+                var layout: [RowLayout]? = nil
+                var itemGroupLayout: GroupLayout? = nil
+                XCTAssertNoThrow(itemGroupLayout = itemLayout as? GroupLayout)
+                XCTAssertNotNil(itemGroupLayout?.getCode())
+                XCTAssertNotNil(layout = itemGroupLayout?.getLayout())
+                XCTAssertGreaterThanOrEqual((layout?.count)!, 0)
+                testGroupLayoutSuccess(layout)
+                break
+            }
+        }
+    }
+    
+    func testGroupLayoutSuccess(_ rowLayouts: [RowLayout]?)
+    {
+        var fields: [FieldLayout]? = nil
+        for rowLayout in rowLayouts!
+        {
+            var itemLayoutType: LayoutType? = nil
+            XCTAssertNotNil(itemLayoutType = rowLayout.getType()!)
+            XCTAssertTrue(type(of: itemLayoutType!) == LayoutType.self)
+            switch itemLayoutType!
+            {
+                case LayoutType.ROW:
+                    var itemRowLayout: RowLayout? = nil
+                    XCTAssertNoThrow(itemRowLayout = rowLayout as RowLayout)
+                    XCTAssertNotNil(fields = itemRowLayout?.getFields())
+                    XCTAssertGreaterThanOrEqual((fields?.count)!, 1)
+                    break
+                case LayoutType.SUBTABLE:
+                    XCTFail()
+                    break
+                case LayoutType.GROUP:
+                    XCTFail()
+                    break
+            }
+        }
+    }
+    
+    func testGetAppFormLayoutFailWhenAppIDNotExist()
+    {
+        let appId = 99999
+        XCTAssertThrowsError(try self.app?.getFormLayout(appId))
+        {
+            error in XCTAssert(type(of: error) == KintoneAPIException.self)
+        }
+    }
+    
+    
 }
--- kintone-ios-sdkTests/module/app/UpdateFormLayoutTest.swift
+++ kintone-ios-sdkTests/module/app/UpdateFormLayoutTest.swift
@@ -1,32 +1,176 @@
-//
-//  UpdateFormLayoutTest.swift
-//  kintone-ios-sdkTests
-//
-//  Created by Pham Anh Quoc Phien on 10/8/18.
 //  Copyright © 2018 Cybozu. All rights reserved.
-//
 
 import XCTest
+import kintone_ios_sdk
 
 class UpdateFormLayoutTest: XCTestCase {
 
+    private let USERNAME = "Phien"
+    private let PASSWORD = "Phien"
+    private let APP_ID = 1687
+    private let LANG = LanguageSetting.EN
+    
+    private var app: App? = nil
+    private var connection: Connection? = nil
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        var auth = Auth.init()
+        auth = auth.setPasswordAuth(self.USERNAME, self.PASSWORD)
+        self.connection = Connection(TestsConstants.DOMAIN, auth)
+        self.app = App(self.connection!)
     }
+
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testUpdateAppFormLayoutSuccess()
+    {
+        var itemLayoutRequest: [ItemLayout]? = [ItemLayout]()
+     
+        // Row Layout
+        let rowLayout1: RowLayout? = RowLayout()
+        var fieldsRowLayout1: [FieldLayout]? = [FieldLayout]()
+        
+        let singleFieldRowLayout1: FieldLayout? = FieldLayout()
+        singleFieldRowLayout1?.setCode("Text")
+        singleFieldRowLayout1?.setType(FieldType.SINGLE_LINE_TEXT.rawValue)
+        let singleFieldSizeRowLayout1: FieldSize? = FieldSize()
+        singleFieldSizeRowLayout1?.setWidth("193")
+        singleFieldRowLayout1?.setSize(singleFieldSizeRowLayout1)
+        fieldsRowLayout1?.append(singleFieldRowLayout1!)
+     
+        let richTextFieldRowLayout1: FieldLayout? = FieldLayout()
+        richTextFieldRowLayout1?.setCode("Rich_text")
+        richTextFieldRowLayout1?.setType(FieldType.RICH_TEXT.rawValue)
+        let richTextFieldSizeRowLayout1: FieldSize? = FieldSize()
+        richTextFieldSizeRowLayout1?.setWidth("315")
+        richTextFieldSizeRowLayout1?.setInnerHeight("125")
+        richTextFieldRowLayout1?.setSize(richTextFieldSizeRowLayout1)
+        fieldsRowLayout1?.append(richTextFieldRowLayout1!)
+        
+        rowLayout1?.setFields(fieldsRowLayout1)
+        
+        // Subtable Layout
+        let subTableLayout: SubTableLayout? = SubTableLayout()
+        var fieldSubTableLayout: [FieldLayout]? = [FieldLayout]()
+        
+        let singleFieldSubTableLayout1: FieldLayout? = FieldLayout()
+        singleFieldSubTableLayout1?.setCode("Text_0")
+        singleFieldSubTableLayout1?.setType(FieldType.SINGLE_LINE_TEXT.rawValue)
+        let singleFieldSizeSubTableLayout1: FieldSize? = FieldSize()
+        singleFieldSizeSubTableLayout1?.setWidth("193")
+        singleFieldSubTableLayout1?.setSize(singleFieldSizeSubTableLayout1)
+        
+        fieldSubTableLayout?.append(singleFieldSubTableLayout1!)
+        subTableLayout?.setFields(fieldSubTableLayout)
+        subTableLayout?.setCode("Table")
+        
+        // GROUP Layout
+        let groupLayout: GroupLayout? = GroupLayout()
+        var rowLayoutInGroup: [RowLayout]? = [RowLayout]()
+        // Row Layout
+        let firstRowLayoutInGroup: RowLayout? = RowLayout()
+        var fieldsInRowLayoutInGroup: [FieldLayout]? = [FieldLayout]()
+        // Numeric Field Layout
+        let numericFieldInRowLayoutInGroup: FieldLayout? = FieldLayout()
+        numericFieldInRowLayoutInGroup?.setCode("Number")
+        numericFieldInRowLayoutInGroup?.setType(FieldType.NUMBER.rawValue)
+        // field size
+        let numericFieldSizeInRowLayoutInGroup: FieldSize? = FieldSize()
+        numericFieldSizeInRowLayoutInGroup?.setWidth("200")
+        numericFieldInRowLayoutInGroup?.setSize(numericFieldSizeInRowLayoutInGroup)
+        
+        fieldsInRowLayoutInGroup?.append(numericFieldInRowLayoutInGroup!)
+        firstRowLayoutInGroup?.setFields(fieldsInRowLayoutInGroup)
+        rowLayoutInGroup?.append(firstRowLayoutInGroup!)
+        groupLayout?.setLayout(rowLayoutInGroup)
+        groupLayout?.setCode("Field_group")
+        
+        // Append layout
+        itemLayoutRequest?.append(rowLayout1!)
+        itemLayoutRequest?.append(subTableLayout!)
+        itemLayoutRequest?.append(groupLayout!)
+        
+        var basicResponse: BasicResponse? = nil
+        XCTAssertNoThrow(basicResponse = try self.app?.updateFormLayout(self.APP_ID, itemLayoutRequest))
+        XCTAssertNotNil(basicResponse?.getRevision())
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testUpdateAppFormLayoutFailWhenAppIdNotExist()
+    {
+        let appId = 99999
+        var itemLayoutRequest: [ItemLayout]? = [ItemLayout]()
+        
+        // Row Layout
+        let rowLayout1: RowLayout? = RowLayout()
+        var fieldsRowLayout1: [FieldLayout]? = [FieldLayout]()
+        
+        let singleFieldRowLayout1: FieldLayout? = FieldLayout()
+        singleFieldRowLayout1?.setCode("Text")
+        singleFieldRowLayout1?.setType(FieldType.SINGLE_LINE_TEXT.rawValue)
+        let singleFieldSizeRowLayout1: FieldSize? = FieldSize()
+        singleFieldSizeRowLayout1?.setWidth("193")
+        singleFieldRowLayout1?.setSize(singleFieldSizeRowLayout1)
+        fieldsRowLayout1?.append(singleFieldRowLayout1!)
+        
+        let richTextFieldRowLayout1: FieldLayout? = FieldLayout()
+        richTextFieldRowLayout1?.setCode("Rich_text")
+        richTextFieldRowLayout1?.setType(FieldType.RICH_TEXT.rawValue)
+        let richTextFieldSizeRowLayout1: FieldSize? = FieldSize()
+        richTextFieldSizeRowLayout1?.setWidth("315")
+        richTextFieldSizeRowLayout1?.setInnerHeight("125")
+        richTextFieldRowLayout1?.setSize(richTextFieldSizeRowLayout1)
+        fieldsRowLayout1?.append(richTextFieldRowLayout1!)
+        
+        rowLayout1?.setFields(fieldsRowLayout1)
+        
+        // Subtable Layout
+        let subTableLayout: SubTableLayout? = SubTableLayout()
+        var fieldSubTableLayout: [FieldLayout]? = [FieldLayout]()
+        
+        let singleFieldSubTableLayout1: FieldLayout? = FieldLayout()
+        singleFieldSubTableLayout1?.setCode("Text_0")
+        singleFieldSubTableLayout1?.setType(FieldType.SINGLE_LINE_TEXT.rawValue)
+        let singleFieldSizeSubTableLayout1: FieldSize? = FieldSize()
+        singleFieldSizeSubTableLayout1?.setWidth("193")
+        singleFieldSubTableLayout1?.setSize(singleFieldSizeSubTableLayout1)
+        
+        fieldSubTableLayout?.append(singleFieldSubTableLayout1!)
+        subTableLayout?.setFields(fieldSubTableLayout)
+        subTableLayout?.setCode("Table")
+        
+        // GROUP Layout
+        let groupLayout: GroupLayout? = GroupLayout()
+        var rowLayoutInGroup: [RowLayout]? = [RowLayout]()
+        // Row Layout
+        let firstRowLayoutInGroup: RowLayout? = RowLayout()
+        var fieldsInRowLayoutInGroup: [FieldLayout]? = [FieldLayout]()
+        // Numeric Field Layout
+        let numericFieldInRowLayoutInGroup: FieldLayout? = FieldLayout()
+        numericFieldInRowLayoutInGroup?.setCode("Number")
+        numericFieldInRowLayoutInGroup?.setType(FieldType.NUMBER.rawValue)
+        // field size
+        let numericFieldSizeInRowLayoutInGroup: FieldSize? = FieldSize()
+        numericFieldSizeInRowLayoutInGroup?.setWidth("200")
+        numericFieldInRowLayoutInGroup?.setSize(numericFieldSizeInRowLayoutInGroup)
+        
+        fieldsInRowLayoutInGroup?.append(numericFieldInRowLayoutInGroup!)
+        firstRowLayoutInGroup?.setFields(fieldsInRowLayoutInGroup)
+        rowLayoutInGroup?.append(firstRowLayoutInGroup!)
+        groupLayout?.setLayout(rowLayoutInGroup)
+        groupLayout?.setCode("Field_group")
+        
+        // Append layout
+        itemLayoutRequest?.append(rowLayout1!)
+        itemLayoutRequest?.append(subTableLayout!)
+        itemLayoutRequest?.append(groupLayout!)
+        
+        XCTAssertThrowsError(try self.app?.updateFormLayout(appId, itemLayoutRequest))
+        {
+            error in XCTAssert(type(of: error) == KintoneAPIException.self)
         }
     }
 
