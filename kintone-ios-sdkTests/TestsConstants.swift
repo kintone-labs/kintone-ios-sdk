7e293d96a8177299cc75be861b188f0785afd159
Nguyen Nhat <n001417@pc-001417-1.local>
2018-12-18 10:00:47 +0700

    update constant for test

--- kintone-ios-sdkTests/TestsConstants.swift
+++ kintone-ios-sdkTests/TestsConstants.swift
@@ -7,9 +7,12 @@
 //
 
 public class TestsConstants {
-    public static let DOMAIN = "https://phienphamf19-2-1.s.cybozu-dev.com"
-    public static let ADMIN_USERNAME = "dinhnhat"
-    public static let ADMIN_PASSWORD = "phongnhat@23101995"
+    public static let DOMAIN = "YOUR_DOMAIN"
+    public static let ADMIN_USERNAME = "YOUR_USERNAME"
+    public static let ADMIN_PASSWORD = "YOUR_PASSWORD"
+    public static let CERT_NAME = "YOUR_CERT_NAME"
+    public static let CERT_PASSWORD = "YOUR_CERT_PASSWORD"
+    public static let CERT_EXTENSION = "YOUR_CERT_EXTENSION"
     public static let PROXY_HOST = "10.224.136.41"
     public static let PROXY_PORT = 3128
     public static let GUEST_SPACE_ID = 4
