305af0eb45a2ba274fc1c394e6a695251894a718
Vu Tran <vutranshield@gmail.com>
2019-04-19 09:38:30 +0700

    updated TestsConstants

--- kintone-ios-sdkTests/TestsConstants.swift
+++ kintone-ios-sdkTests/TestsConstants.swift
@@ -7,9 +7,9 @@
 //
 
 public class TestsConstants {
-    public static let DOMAIN = "YOUR_DOMAIN"
-    public static let ADMIN_USERNAME = "YOUR_USERNAME"
-    public static let ADMIN_PASSWORD = "YOUR_PASSWORD"
+    public static let DOMAIN = "qasd-vu.cybozu-dev.com"
+    public static let ADMIN_USERNAME = "cybozu"
+    public static let ADMIN_PASSWORD = "vutran123"
     public static let CERT_NAME = "YOUR_CERT_NAME"
     public static let CERT_PASSWORD = "YOUR_CERT_PASSWORD"
     public static let CERT_EXTENSION = "YOUR_CERT_EXTENSION"
