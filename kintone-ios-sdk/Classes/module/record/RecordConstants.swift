2bcaa8f8df1e9a1a37de02f50e75be8e17e7c248
minhhy2801 <minhhy2897@gmail.com>
2019-08-30 08:39:24 +0700

    merge common RecordConstants

--- kintone-ios-sdk/Classes/module/record/RecordConstants.swift
+++ kintone-ios-sdk/Classes/module/record/RecordConstants.swift
@@ -1,14 +1,5 @@
-//
-//  RecordConstants.swift
-//  kintone-ios-sdk
-//
-//  Created by Cybozu on 8/28/19.
-//  Copyright © 2019 Cybozu. All rights reserved.
-//
-
 open class RecordConstants: NSObject {
     public static let LIMIT_GET_RECORD = 500
     public static let LIMIT_DELETE_RECORD = 100
     public static let NUM_BULK_REQUEST = 20
 }
-
