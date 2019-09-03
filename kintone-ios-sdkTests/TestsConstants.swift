9a7495ff52c043d28f671eea6315bfd5631127e4
minhhy2801 <minhhy2897@gmail.com>
2019-09-03 09:23:39 +0700

    test constants update

--- kintone-ios-sdkTests/TestsConstants.swift
+++ kintone-ios-sdkTests/TestsConstants.swift
@@ -13,8 +13,10 @@ public class TestsConstants {
     public static let CERT_NAME = "YOUR_CERT_NAME"
     public static let CERT_PASSWORD = "YOUR_CERT_PASSWORD"
     public static let CERT_EXTENSION = "YOUR_CERT_EXTENSION"
-    public static let PROXY_HOST = "XXX.XXX.XXX.XXX"
+    public static let PROXY_HOST = "xxx"
     public static let PROXY_PORT = 1234
+    public static let PROXY_USERNAME = "xxx"
+    public static let PROXY_PASSWORD = "xxx"
     public static let GUEST_SPACE_ID = 1234
     public static let MAX_VALUE = 2147483647
 }
