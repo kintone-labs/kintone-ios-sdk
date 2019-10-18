5a698044ac899f25dd76f454091e842fca8bb1bc
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-18 16:01:23 +0700

    test

--- kintone-ios-sdk/Classes/authentication/AuthenticationConstants.swift
+++ kintone-ios-sdk/Classes/authentication/AuthenticationConstants.swift
@@ -5,10 +5,11 @@
 //  Created by t000572 on 2018/09/19.
 //  Copyright © 2018年 Cybozu. All rights reserved.
 //
-
 open class AuthenticationConstants: NSObject {
     public static let HEADER_KEY_AUTH_PASSWORD = "X-Cybozu-Authorization"
     public static let HEADER_KEY_AUTH_APITOKEN = "X-Cybozu-API-Token"
     public static let HEADER_KEY_AUTH_BASIC = "Authorization"
     public static let AUTH_BASIC_PREFIX = "Basic "
+    public static let AUTH_BASIC_PREFIX1 = "Basic 1"
+
 }
