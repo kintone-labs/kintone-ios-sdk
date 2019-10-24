53ed3345afc47a22bc07c189fdcf88c097ad3a7d
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-24 13:40:51 +0700

    check version podspec

---
+++ actions/check_version/index.js
@@ -0,0 +1,19 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { exec } = require("child_process");
+try {
+  exec("pod ipc spec kintone-ios-sdk.podspec", function(err, stdout, stderr) {
+    if (err) {
+      core.setFailed(err);
+    }
+    const tagVersion = github.context.payload.release && github.context.payload.release.name;
+    let packageVersion = "v" + JSON.parse(stdout).version;
+    if (tagVersion !== packageVersion) {
+      core.setFailed("Version is invalid");
+    } else {
+      core.setOutput("version", packageVersion);
+    }
+  });
+} catch (e) {
+  core.setFailed(e.message);
+}
