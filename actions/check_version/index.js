1a6f375948ef54db7e87c9eb0205743e056fdcf6
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-28 10:17:25 +0700

    add input podspec file to action yml

--- actions/check_version/index.js
+++ actions/check_version/index.js
@@ -4,19 +4,18 @@ const { exec } = require("child_process");
 
 try {
   let podspec = core.getInput("podspec");
-  console.log("podFile",podspec);
-  // exec(`pod ipc spec ${podspec}`, function(err, stdout, stderr) {
-  //   if (err) {
-  //     core.setFailed(err);
-  //   }
-  //   const tagVersion = github.context.payload.release && github.context.payload.release.name;
-  //   let packageVersion = "v" + JSON.parse(stdout).version;
-  //   if (tagVersion !== packageVersion) {
-  //     core.setFailed("Version is invalid");
-  //   } else {
-  //     core.setOutput("version", packageVersion);
-  //   }
-  // });
+  exec(`pod ipc spec ${podspec}`, function(err, stdout, stderr) {
+    if (err) {
+      core.setFailed(err);
+    }
+    const tagVersion = github.context.payload.release && github.context.payload.release.name;
+    let packageVersion = "v" + JSON.parse(stdout).version;
+    if (tagVersion !== packageVersion) {
+      core.setFailed("Version is invalid");
+    } else {
+      core.setOutput("version", packageVersion);
+    }
+  });
 } catch (e) {
   core.setFailed(e.message);
 }
