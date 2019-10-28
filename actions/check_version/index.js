d35b47e0fef98b798b931b67870adc3e4f82629d
Pham-Gia-Huong <phamgiahuong2912@gmail.com>
2019-10-28 10:08:22 +0700

    add default input pod

--- actions/check_version/index.js
+++ actions/check_version/index.js
@@ -3,20 +3,20 @@ const github = require("@actions/github");
 const { exec } = require("child_process");
 
 try {
-  let podFile = core.getInput("podFile");
-  console.log(podFile);
-  exec(`pod ipc spec ${podFile}`, function(err, stdout, stderr) {
-    if (err) {
-      core.setFailed(err);
-    }
-    const tagVersion = github.context.payload.release && github.context.payload.release.name;
-    let packageVersion = "v" + JSON.parse(stdout).version;
-    if (tagVersion !== packageVersion) {
-      core.setFailed("Version is invalid");
-    } else {
-      core.setOutput("version", packageVersion);
-    }
-  });
+  let podspec = core.getInput("podspec");
+  console.log("podFile",podspec);
+  // exec(`pod ipc spec ${podspec}`, function(err, stdout, stderr) {
+  //   if (err) {
+  //     core.setFailed(err);
+  //   }
+  //   const tagVersion = github.context.payload.release && github.context.payload.release.name;
+  //   let packageVersion = "v" + JSON.parse(stdout).version;
+  //   if (tagVersion !== packageVersion) {
+  //     core.setFailed("Version is invalid");
+  //   } else {
+  //     core.setOutput("version", packageVersion);
+  //   }
+  // });
 } catch (e) {
   core.setFailed(e.message);
 }
